4b8e556fe48215a8fa466e59007d18beda624bbf
Brian Ketelsen <bketelsen@gmail.com>
2022-11-28 06:05:08 -0500

    more cleanup

--- __setup/common/01-zsh.sh
+++ __setup/common/01-zsh.sh
@@ -3,4 +3,4 @@
 # append brew's zsh to the allowed shells
 echo "/home/linuxbrew/.linuxbrew/bin/zsh" | sudo tee -a /etc/shells
 
-sudo chsh -s /home/linuxbrew/.linuxbrew/bin/zsh bjk
+sudo chsh -s /home/linuxbrew/.linuxbrew/bin/zsh `whoami`
